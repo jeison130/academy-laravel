e93778d31f2db54a132e484a414d5ec6c7eab8b6
jeison130 <jeison130@gmail.com>
2020-10-21 21:24:46 -0500

    Migraciones

--- database/migrations/2020_10_22_021821_create_courses_table.php
+++ database/migrations/2020_10_22_021821_create_courses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStudentsTable extends Migration
+class CreateCoursesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
-            $table->string('lastName', 50);
-            $table->integer('age');
-            $table->string('email', 50);
+            $table->string('name', 255);
+            $table->string('schedule', 255);
+            $table->date('start_date');
+            $table->date('end_date');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('courses');
     }
 }
